ecec9bfc50636853b96c686f274e7e7f3056938e
César Martins <cesar.martins@happycodeschool.com>
2019-03-26 16:48:55 -0700

    added blueDuck

--- public/scripts/canvastest.js
+++ public/scripts/canvastest.js
@@ -7,7 +7,7 @@ var myGamePiece;
 window.onload = function startGame() {
   console.log('start game called!');
   myGameArea.start();
-  myGamePiece = new component(30, 30, "red", 10, 120);
+  myGamePiece = new component(81, 81, "images/builder-assets/pato_azul_A25_Rectangle_28_pattern.png", 10, 120, "image");
 }
 
 
@@ -19,21 +19,25 @@ var myGameArea = {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
+    //---This is for Touch
     window.addEventListener('touchmove', function (e) {
       console.log(e, 'this is the (e)Vent');
       myGameArea.x = e.touches[0].screenX;
       myGameArea.y = e.touches[0].screenY;
     })
+    //---This is for Touch
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
 }
 
-
-
-
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+  this.type = type;
+  if (type == "image") {
+  this.image = new Image();
+  this.image.src = color;
+  }
   this.width = width;
   this.height = height;
   this.speedX = 0;
@@ -42,8 +46,15 @@ function component(width, height, color, x, y) {
   this.y = y;
   this.update = function() {
     ctx = myGameArea.context;
+    if (type == "image") {
+      ctx.drawImage(this.image,
+        this.x,
+        this.y,
+        this.width, this.height);
+    } else {
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
   this.newPos = function() {
     this.x += this.speedX;
@@ -51,12 +62,16 @@ function component(width, height, color, x, y) {
   }
 }
 
+
 function updateGameArea() {
   myGameArea.clear();
+  //---This is for Touch
   if (myGameArea.x && myGameArea.y) {
   myGamePiece.x = myGameArea.x;
   myGamePiece.y = myGameArea.y;
   }
+  //---This is for Touch
+
   // myGamePiece.newPos();
   myGamePiece.update();
 }
